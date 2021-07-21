c53a7fa87bf90275b1916b0e860bc012f659c81d
Star Coder <starcoder204@gmail.com>
2021-07-21 18:32:18 +0200

    first commit

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="dashboard-page">
+        <h1> Dashboard </h1>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
