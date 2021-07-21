238f2bf825448152d1dec7a35522dfd98048dcbd
Star Coder <starcoder204@gmail.com>
2021-07-21 18:41:29 +0200

    setup bootstrap library

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,11 +1,24 @@
 <template>
     <div class="dashboard-page">
-        <h1> Dashboard </h1>
+        <b-container>
+            <h1> Dashboard </h1>
+            <b-table striped hover :items="items"></b-table>
+        </b-container>
     </div>
 </template>
 
 <script>
 export default {
+    data() {
+      return {
+        items: [
+          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+          { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+          { age: 38, first_name: 'Jami', last_name: 'Carney' }
+        ]
+      }
+    }
 }
 </script>
 
